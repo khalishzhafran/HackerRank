626f6d7659b9a6464eb6756b5bc47eee856c60f7
Khalish-Litsch <khalish260303@gmail.com>
2022-04-25 11:58:12 +0800

    added 15 sql challenges

---
+++ SQL/Weather Observation Station 12.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u') AND LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
---
+++ SQL/Weather Observation Station 7.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) IN ('a', 'e', 'i', 'o', 'u');
